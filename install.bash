b91077c9a598672069ebe0be860af268a41f6384
Piotr Yordanov <yordanov.piotr@gmail.com>
2012-03-01 18:38:53 +0200

    Adding byobu and install script

---
+++ install.bash
@@ -0,0 +1,15 @@
+DIR=$(pwd)
+    
+# VIM
+ln -s $DIR/vim/.vim ~/
+ln -s $DIR/vim/.vimrc ~/
+
+# MC
+ln -s $DIR/mc/.mc ~/
+
+# BASH
+ln -s $DIR/bashrc/.bash_functions.bash ~/
+
+# Byobu
+ln -s $DIR/byobu/.byobu ~/
+ln -s $DIR/byobu/.screenrc ~/
